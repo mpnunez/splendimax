f2a74943092f85d802e8f493373f23b459a65a1d
Marcel Nunez <marcelpnunez@gmail.com>
2025-01-26 10:16:48 -0800

    Read csv and deserialize into Card format

--- src/bin/read-cards.rs
+++ src/bin/read-cards.rs
@@ -1,13 +1,72 @@
 use std::{error::Error, io, process};
 
+extern crate splendimax;
+use splendimax::color::Color;
+use splendimax::cost::Tokens;
+use splendimax::card::Card;
+
+
+
+// By default, struct field names are deserialized based on the position of
+// a corresponding field in the CSV data's header record.
+#[derive(Debug, serde::Deserialize)]
+struct CardRecord {
+    #[serde(rename = "Level")]
+    level: u8,
+    #[serde(rename = "Color")]
+    color: String,
+    #[serde(rename = "PV")]
+    pv: u8,
+    #[serde(rename = "Black")]
+    black: u8,
+    #[serde(rename = "Blue")]
+    blue: u8,
+    #[serde(rename = "Green")]
+    green: u8,
+    #[serde(rename = "Red")]
+    red: u8,
+    #[serde(rename = "White")]
+    white: u8,
+}
+
+impl CardRecord {
+
+    fn get_color(&self) -> Color {
+        match self.color.as_ref() {
+            "Black" => Color::Black,
+            "Blue" => Color::Blue,
+            "Green" => Color::Green,
+            "Red" => Color::Red,
+            "White" => Color::White,
+            _ => panic!("Unknown color {}", self.color),
+        }
+    }
+
+    fn create_card(&self) -> Card {
+        Card {
+            color: self.get_color(),
+            cost: Tokens {
+                black: self.black,
+                blue: self.blue,
+                green: self.green,
+                red: self.red,
+                white: self.white,
+                joker: 0,
+            },
+            points: self.pv,
+        }
+    }
+}
+
 fn example() -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_path("cards.csv")?;
-    for result in rdr.records() {
+    for result in rdr.deserialize() {
         // The iterator yields Result<StringRecord, Error>, so we check the
         // error here.
-        let record = result?;
-        println!("{:?}", record);
+        let record: CardRecord = result?;
+        let card = record.create_card();
+        println!("{:?}", card);
     }
     Ok(())
 }
